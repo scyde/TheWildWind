f12cb9e5ab4d3fd65f251e64992a9182b2ac9362
Scyde <scyde.x3@gmail.com>
2018-04-08 17:36:14 +0700

    react-router-redux basic implementation

--- models/reviews.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const reviewSchema = mongoose.Schema({
-    text: String,
-    author: String
-})
-
-module.exports = mongoose.model('Review',reviewSchema)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader', 'postcss-loader'
+                ]
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    watch: true,
+    watchOptions: {
+        //  ignored: /node_modules/
+    }
+}
