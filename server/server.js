0f87151dbbe5ab424a9c90eac26fd54d19453a83
Scyde <scyde.x3@gmail.com>
2018-04-13 08:34:24 +0700

    Added SSR for main file Merged loopback and own servers into one Refactored images

--- server/server.js
+++ server/server.js
@@ -1,29 +1,36 @@
+const path = require('path')
 'use strict'
 
-var loopback = require('loopback')
-var boot = require('loopback-boot')
+const loopback = require('loopback')
+const boot = require('loopback-boot')
 
-var app = module.exports = loopback()
+const app = module.exports = loopback()
+
+app.use(loopback.static(path.resolve(__dirname, '../public')))
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    app.emit('started')
+    const baseUrl = app.get('url').replace(/\/$/, '')
+    console.log('Web server listening at: %s', baseUrl)
     if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      const explorerPath = app.get('loopback-component-explorer').mountPath
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath)
     }
-  });
-};
+  })
+}
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+  if (err) throw err
 
+  app.set('view engine','ejs')
+    app.get('/*',(req,res) => {
+        res.render('index')
+    })
   // start the server if `$ node server.js`
   if (require.main === module)
-    app.start();
-});
+    app.start()
+})
